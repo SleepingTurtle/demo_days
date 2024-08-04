4382364b7912312a82619640047d8939b5b0fc32
Joshua Charles <josh@asleepingturtle.com>
2024-08-04 01:29:51 -0400

    Lint updates for main

--- main.py
+++ main.py
@@ -2,13 +2,11 @@ import requests
 
 
 def fetchWeather(lat, lon):
-    response =  requests.get(f"https://api.weather.gov/points/{lat},{lon}")  .json()
+    response = requests.get(f"https://api.weather.gov/points/{lat},{lon}").json()
     x = response["@context"][1]["@version"]
     print("EVERTHING CAPS")
     return x
 
 
 if __name__ == "__main__":
-    print(fetchWeather('41.409','-75.6624'))
-
-
+    print(fetchWeather("41.409", "-75.6624"))
