e11e0213124f2cb9009f91d4a56c85fd45c07d81
Ryan Long <rlong@wasabi.com>
2024-08-08 20:26:52 -0400

    deleted weather

--- main.py
+++ main.py
@@ -7,15 +7,6 @@ def fetchWeatherAPIVersion(lat, lon):
     print("EVERTHING CAPS")
     return x
 
-
-def fetchWeather(lat, lon):
-    response = requests.get(f"https://api.weather.gov/points/{lat},{lon}").json()
-    hourlyLink = response["properties"]["forecastHourly"]
-    response = requests.get(f"{hourlyLink}").json()
-    hourTemp = response["properties"]["periods"][0]["temperature"]
-    return hourTemp
-
-
 if __name__ == "__main__":
 
     fetchWeatherAPIVersion(44, 22)
